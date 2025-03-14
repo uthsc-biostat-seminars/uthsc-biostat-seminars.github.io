c40f0ce59f14381e77bac05aa129ea38d3fdb6b6
GregFa <36859310+GregFa@users.noreply.github.com>
2025-03-14 18:59:08 -0500

    Updated open accordion

--- docs/generate_seminar_htlm.py
+++ docs/generate_seminar_htlm.py
@@ -153,7 +153,10 @@ for year in years:
             df['Affiliation'][i] = df['Affiliation'][i].replace('"', '')
             df['Title'][i] = df['Title'][i].replace('"', '')
             df['Abstract'][i] = df['Abstract'][i].replace('"', '')
-            html += f'<button class="accordion">{df["Date"][i]}, {df["Speaker"][i]}, {df["Affiliation"][i]}, {df["Title"][i]}</button>\n<div class="panel">\n<p>{df["Abstract"][i]}</p>\n</div>\n'
+            if i == len(df)-1 and year == years[0]:
+              html += f'<button class="accordion active">{df["Date"][i]}, {df["Speaker"][i]}, {df["Affiliation"][i]}, {df["Title"][i]}</button>\n<div class="panel" style="display: block;">\n<p>{df["Abstract"][i]}</p>\n</div>\n'
+            else:
+              html += f'<button class="accordion">{df["Date"][i]}, {df["Speaker"][i]}, {df["Affiliation"][i]}, {df["Title"][i]}</button>\n<div class="panel">\n<p>{df["Abstract"][i]}</p>\n</div>\n'
     html += '</div>\n'
 
 # add </body> </html>
